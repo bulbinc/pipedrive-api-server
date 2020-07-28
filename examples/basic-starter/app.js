599e44998d43662d6ac0f2ba3bab331aea486a05
mizunoajigawakaru <rytfksm@gmail.com>
2020-07-28 20:10:16 +0900

    init

--- examples/basic-starter/app.js
+++ examples/basic-starter/app.js
@@ -1,4 +1,7 @@
 'use strict'
+require('dotenv').config()
+
+const pipedrive = require('pipedrive')
 const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
@@ -9,6 +12,8 @@ const app = express()
 const router = express.Router()
 
 app.set('view engine', 'pug')
+
+pipedrive.Configuration.apiToken = process.env.PIPEDRIVE_API_TOKEN;
 
 if (process.env.NODE_ENV === 'test') {
   // NOTE: aws-serverless-express uses this app for its integration tests
@@ -27,9 +32,10 @@ router.use(awsServerlessExpressMiddleware.eventContext())
 app.set('views', path.join(__dirname, 'views'))
 
 router.get('/', (req, res) => {
-  res.render('index', {
-    apiUrl: req.apiGateway ? `https://${req.apiGateway.event.headers.Host}/${req.apiGateway.event.requestContext.stage}` : 'http://localhost:3000'
-  })
+  res.json({ message: 'Welcome to Bulb Pipedrive API' })
+  // res.render('index', {
+  //   apiUrl: req.apiGateway ? `https://${req.apiGateway.event.headers.Host}/${req.apiGateway.event.requestContext.stage}` : 'http://localhost:3000'
+  // })
 })
 
 router.get('/sam', (req, res) => {
@@ -88,10 +94,92 @@ const users = [{
 }]
 let userIdCounter = users.length
 
+// start pipedrive
+router.options('*', cors())
+
+router.post('/contact', cors(), async (req, res, next) => {
+  console.log('/contact', req.body.name, req.body.email)
+
+  let person;
+  try {
+    const { name, email, phone } = req.body;
+    const personInput = {
+      body: {
+        name,
+        email: [email],
+        phone: [phone],
+      }
+    };
+    const { data } = await pipedrive.PersonsController.addAPerson(personInput);
+    person = data;
+  } catch (error) {
+    next(error);
+  }
+
+  let organization;
+  try {
+    const { organization_name } = req.body;
+    const organizationInput = {
+      body: {
+        name: organization_name
+      }
+    };
+
+    const { data } = await pipedrive.OrganizationsController.addAnOrganization(organizationInput);
+    organization = data;
+  } catch (error) {
+    next(error);
+  }
+
+  let deal;
+  try {
+    const { title, budget } = req.body;
+    const dealInput = {
+      body: {
+        title,
+        value: budget,
+        person_id: person.id,
+        org_id: organization.id,
+      }
+    };
+
+    const { data } = await pipedrive.DealsController.addADeal(dealInput);
+    deal = data;
+  } catch (error) {
+    next(error);
+  }
+
+  let note;
+  try {
+    const { contact_body } = req.body;
+    const noteInput = {
+      content: contact_body,
+      dealId: deal.id,
+      personId: person.id,
+    };
+    const { data } = await pipedrive.NotesController.addANote(noteInput);
+    note = data;
+  } catch (error) {
+    next(error);
+  }
+
+  res.send({ message: 'success!' });
+});
+// end pipedrive
+
+
 // The aws-serverless-express library creates a server and listens on a Unix
 // Domain Socket for you, so you can remove the usual call to app.listen.
 // app.listen(3000)
 app.use('/', router)
+
+function errorHandler(err, req, res, next) {
+  // console.log('errorHandler', err);
+  res.status(err.errorCode || 500);
+  res.send({ error: err })
+}
+
+app.use(errorHandler)
 
 // Export your express server so you can import it in the lambda function.
 module.exports = app
