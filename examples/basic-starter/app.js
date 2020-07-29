207a961c9452b06f0fe90a05330e1ed7ed6cdae1
mizunoajigawakaru <rytfksm@gmail.com>
2020-07-29 23:47:06 +0900

    add slack webhook

--- examples/basic-starter/app.js
+++ examples/basic-starter/app.js
@@ -2,6 +2,7 @@
 require('dotenv').config()
 
 const pipedrive = require('pipedrive')
+const { IncomingWebhook } = require('@slack/webhook')
 const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
@@ -11,9 +12,61 @@ const awsServerlessExpressMiddleware = require('aws-serverless-express/middlewar
 const app = express()
 const router = express.Router()
 
+// slack webhook
+const url = process.env.SLACK_WEBHOOK_URL
+const webhook = new IncomingWebhook(url)
+
 app.set('view engine', 'pug')
 
 pipedrive.Configuration.apiToken = process.env.PIPEDRIVE_API_TOKEN;
+
+function getSuccessResultText(req) {
+  const {
+    name,
+    email,
+    phone,
+    organization_name,
+    budget,
+    contact_body
+  } = req.body
+
+  return `
+新規問い合わせ
+--------------------------------------
+名前: ${name}
+メールアドレス: ${email}
+電話番号: ${phone}
+会社名: ${organization_name}
+予算: ${budget}
+問い合わせ内容:
+${contact_body}
+`
+}
+
+function getErrorResultText(req) {
+  const {
+    name,
+    email,
+    phone,
+    organization_name,
+    budget,
+    contact_body
+  } = req.body
+
+  return `
+PIPEDRIVE API SERVERでエラーが発生しました
+--------------------------------------
+以下送信内容
+
+名前: ${name}
+メールアドレス: ${email}
+電話番号: ${phone}
+会社名: ${organization_name}
+予算: ${budget}
+問い合わせ内容:
+${contact_body}
+`
+}
 
 if (process.env.NODE_ENV === 'test') {
   // NOTE: aws-serverless-express uses this app for its integration tests
@@ -46,7 +99,7 @@ router.post('/contact', cors(), async (req, res, next) => {
         phone: [phone],
       }
     };
-    const { data } = await pipedrive.PersonsController.addAPerson(personInput);
+    const { data } = await pipedrive.PersonsController.addAPerson({});
     person = data;
   } catch (error) {
     next(error);
@@ -99,6 +152,14 @@ router.post('/contact', cors(), async (req, res, next) => {
     next(error);
   }
 
+  try {
+    await webhook.send({
+      text: getSuccessResultText(req)
+    });
+  } catch (error) {
+    next(error);
+  }
+
   res.send({ message: 'success!' });
 });
 // end pipedrive
@@ -112,6 +173,10 @@ function errorHandler(err, req, res, next) {
   // console.log('errorHandler', err);
   res.status(err.errorCode || 500);
   res.send({ error: err })
+
+  webhook.send({
+    text: getErrorResultText(req)
+  });
 }
 
 app.use(errorHandler)
